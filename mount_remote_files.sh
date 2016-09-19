b6f385a8233a3bc32543eae898291f3cb1cd064e
Jeronimo Grandi <jggrandi@gmail.com>
2016-09-19 12:45:36 -0300

    Extract video frame, disable network card, mount remote files

---
+++ mount_remote_files.sh
@@ -0,0 +1,3 @@
+sudo mount -t nfs -o nolock 192.168.111.2:/mnt/Data/GeneralData /home/jeronimo/XmoNas_Files/GeneralData
+sudo mount -t nfs -o nolock 192.168.111.2:/mnt/Data/UsersHome/AdvancedUsers/Jeronimo /home/jeronimo/XmoNas_Files/Home
+sudo mount -t nfs -o nolock 192.168.111.2:/mnt/Entertainment/ /home/jeronimo/XmoNas_Files/Entertainment
